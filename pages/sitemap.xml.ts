23dcc999c0dd5f8967576310f41ff4926da914e4
primalCam <alphaglobalmedia@gmail.com>
2025-11-21 07:00:27 -0500

    Added sitemap.xml and robots.txt for SEO indexing

---
+++ pages/sitemap.xml.ts
@@ -0,0 +1,75 @@
+// pages/sitemap.xml.ts
+
+import fs from "fs";
+import path from "path";
+import { GetServerSideProps } from "next";
+
+const SITE_URL = "https://notionhighlights.com";
+
+function generateSiteMap(slugs: string[]) {
+  const urls = slugs
+    .map(
+      (slug) => `
+  <url>
+    <loc>${SITE_URL}/blog/${slug}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`
+    )
+    .join("");
+
+  // Add static pages too
+  const staticPages = `
+  <url>
+    <loc>${SITE_URL}</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/upgrade</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/blog</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
+  `;
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xhtml="http://www.w3.org/1999/xhtml"
+  >
+    ${staticPages}
+    ${urls}
+  </urlset>`;
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  // Read all your blog posts from /posts folder
+  const postsDirectory = path.join(process.cwd(), "posts");
+  const files = fs.readdirSync(postsDirectory);
+
+  // Accept both .md and .mdx files
+  const slugs = files
+    .filter((file) => /\.(md|mdx)$/.test(file))
+    .map((file) => file.replace(/\.mdx?$/, ""));
+
+  // Generate the XML sitemap
+  const sitemap = generateSiteMap(slugs);
+
+  // Write sitemap to response
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default function SiteMap() {
+  return null; // Required by Next.js but never rendered
+}
